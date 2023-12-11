f84b639aa0f754832e9a88f0669441d08f2b2ba8
Mahlet Seleshi <mahletseleshi224@gmail.com>
2023-12-11 23:38:01 +0300

    intial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import logo from "./assets/logo.png"
+
+
+function App() {
+  return (
+   <div className="bg-[#160C6E] h-screen">
+    <div className="bg-[#3F21D0] rounded-full flex items-center h-screen">
+        <h1>Hello Wolrd</h1>
+    </div>
+    {/* <nav>
+      <img src ={logo} alt="logo" className="w-fit"/>
+    </nav> */}
+   </div>
+  )
+}
+
+export default App
